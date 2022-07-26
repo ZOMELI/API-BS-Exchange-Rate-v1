b6c929c926825acdc2f2025150636ac8c2577608
Zomeli <73003008+ZOMELI@users.noreply.github.com>
2022-07-25 20:46:53 -0500

    Update Jenkins-delivery-pipeline.groovy

--- devops/jenkins/Jenkins-delivery-pipeline.groovy
+++ devops/jenkins/Jenkins-delivery-pipeline.groovy
@@ -1,5 +1,19 @@
 pipeline {
+    agent any 
+    tools { 
+        maven 'MAVEN' 
+        jdk 'JDK' 
+    }
     stages{
+        stage ('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                ''' 
+            }
+        }
+        
         stage("build"){
                 steps{
                                 sh "mvn -f ${PROJECT_ROOT} clean install"
